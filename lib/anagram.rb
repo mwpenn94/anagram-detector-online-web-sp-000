678e6f375be64651dab68c40089b631a74e54a70
Michael Penn <mwpenn94@gmail.com>
2021-02-18 13:20:34 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(word_array)
+    anagrams = []
+    word_array.each do |match| 
+      word_chars = match.split("") 
+      anagram?_chars = match.split("") 
+      == word.split("").sort
+        if true
+          anagrams << match
+        end
+      end
+    anagrams
+  end
+end
